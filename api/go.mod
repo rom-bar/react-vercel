7e2b86b0780b86e89f9884173d156100c10e8efc
rombar <rombar@pm.me>
2020-06-18 23:12:59 +0700

    Add student handler

--- api/go.mod
+++ api/go.mod
@@ -1,3 +1,5 @@
 module example-date
 
 go 1.12
+
+require github.com/jinzhu/gorm v1.9.14
---
+++ api/student.go
@@ -0,0 +1,39 @@
+package handler
+
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"os"
+
+	"github.com/jinzhu/gorm"
+	_ "github.com/jinzhu/gorm/dialects/mysql"
+)
+
+type student struct {
+	Id    int    `json:”id”`
+	Nama  string `json:”nama”`
+	Umur  int    `json:”umur”`
+	Kelas int    `json:”kelas”`
+}
+
+func Handler(w http.ResponseWriter, r *http.Request) {
+	dbHost := os.Getenv("MYSQL_HOST")
+	dbUser := os.Getenv("MYSQL_USER")
+	dbPassword := os.Getenv("MYSQL_PASSWORD")
+	dbName := os.Getenv("MYSQL_DATABASE")
+	mysqlDSN := fmt.Sprintf("%v:%v@tcp(%v:3306)/%v?charset=utf8&parseTime=True&loc=Local", dbUser, dbPassword, dbHost, dbName)
+	db, err := gorm.Open("mysql", mysqlDSN)
+
+	if err != nil {
+		fmt.Fprintf(w, "Failed to connect DB")
+	}
+
+	defer db.Close()
+
+	students := []student{}
+
+	db.Find(&students)
+
+	json.NewEncoder(w).Encode(students)
+}
